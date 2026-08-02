5207ca48721c73abed0d97420c469c3a4a85f477
Bastien Chapusot <bastien.chapusot@gmx.com>
2026-08-02 18:10:57 +0200

    almost finished

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let c=document.getElementById('myCanvas');
 c.width = 800;
 c.height = 600;
 let ctx=c.getContext('2d');
-ctx.font = "30px serif";
+ctx.font = "20px serif";
 
 // load sprites
 let spr=new Image();
@@ -12,14 +12,27 @@ spr.onload=function() { sprReady=true; };
 spr.src='sprites.jpg';
 
 // init cards
-const cardFaceDown = {x:1765, y:30}; // position of the faced down card in the sprites
+const cardFaceDown = {x:1765, y:26}; // position of the faced down card in the sprites
 const cardSize = {width:117, height:156}; // card size in the sprites
 const deck = createDeck(); // creates a reference for the cards (id, value, color)
 let drawPile = Array.from({ length: 52 }, (_, i) => i + 1);
 let discardPile = [];
 
+// init actions
+const attackSprite = {x:1765, y:208, w:115, h:116};
+const attackPos = {x:300, y:350};
+const shieldSprite = {x:1770, y:378, w:108, h:120};
+const shieldPos = {x:375, y:350};
+const chargeSprite = {x:1786, y:542, w:78, h:128};
+const chargePos = {x: 450, y:350};
+
 // init players
 let players = [];
+let playersColors = ["lightsalmon", "olivedrab", "skyblue", "thistle"];
+let activePlayerIndex = 0;
+
+// init game state
+let GAMESTATE = 0;
 
 // Fisher-Yates Shuffle
 function shuffle(array) {
@@ -44,14 +57,14 @@ function shuffle(array) {
 
 // create a reference deck
 function createDeck() {
-    const suits = ["clubs", "hearts", "spades", "diamonds"];
+    const suits = ["♣", "♥", "♠", "◆"];
     const deck = {};
 
     for (let i = 0; i < suits.length; i++) {
         const suit = suits[i];
         for (let value = 1; value <= 13; value++) {
             const id = i * 13 + value;
-            deck[id] = { id, value, color: suit };
+            deck[id] = { id, value, color: i, suit:suit };
         }
     }
 
@@ -59,7 +72,7 @@ function createDeck() {
 }
 
 // create a new player
-function Player(playerName) {
+function Player(playerName = "") {
 	this.name = playerName;
 	this.hp = [];
 	this.shield = [];
@@ -69,16 +82,18 @@ function Player(playerName) {
 Player.prototype.toString = function playerToString() {
 	return "{" +
 		this.name + "; " +
-		"shield: " + getCardValue(this.shield) + "; " +
-		"hp: [" + getCardValue(this.hp[0]) + "," + getCardValue(this.hp[1])+ "]" +
+		"shield: " + getCardName(this.shield) + "; " +
+		"hp: [" + getCardName(this.hp[0]) + "," + getCardName(this.hp[1])+ "] " +
+		"charges: " + this.charge.length +
 		"}";
 };
 
 // refreshDrawPile : if drawPile is empty; shuffle the discardPile into a new drawPile
 function refreshDrawPile() {
 	if(drawPile.length === 0) {
-		drawPile = discardPile;
-		discardPile = [];
+		while(discardPile.length > 0) {
+			moveCard(discardPile, drawPile);
+		}
 		shuffle(drawPile);
 		return true;
 	} else return false;
@@ -90,7 +105,7 @@ function moveCard(fromPile, toPile, card=fromPile[0]) {
 	if (index !== -1) {
 		const removedCard = fromPile.splice(index, 1)[0];
 		toPile.push(removedCard);
-		console.log("moved " + removedCard + " (" + deck[removedCard].value + " of " + deck[removedCard].color + ")");
+		console.log("moved " + removedCard + " (" + getCardName(removedCard) + ")");
 		return removedCard;
 	} else {
 		console.log("Card not found in the origin pile");
@@ -103,9 +118,39 @@ function getCardValue(card) {
 	return deck[card].value;
 }
 
+// get card color index (0 to 3)
+function getCardColorIndex(card) {
+	return deck[card].color;
+}
+
 // get sum of values in a list of cards
 function getTotal(cards) {
     return cards.reduce((sum, card) => sum + getCardValue(card), 0);
+}
+
+// get a string of a card based of its id
+function getCardName(card) {
+	return getCardValue(card) + " " + deck[card].suit;
+}
+
+// get (x, y) of a card from its index
+function getSpriteCoordFor(card) {
+	const cardValue = getCardValue(card);
+	const cardColorIndex = getCardColorIndex(card);
+
+	return {x:37+133*(cardValue-1), y:26+170*cardColorIndex};
+}
+
+function generatePairsFrom(value) {
+	let pairs = [];
+	// unique pairs means we stop at half the value
+	for (let k = 1; k <= value / 2; k++) {
+		// each pair must contain value-compatible cards (value from 1 to 13)
+		if (value - k <= 13) {
+			pairs.push([k,value-k]);
+		}
+	}
+	return pairs;
 }
 
 // check if a card's value exists in a pile
@@ -141,12 +186,13 @@ function chargeKnight(player) {
 function attackKnight(attackingPlayer, defendingPlayer) {
 	// totalAttack is the sum of the first card from the drawPile and potential charges
 	let totalAttack = getCardValue(drawPile[0]);
-	totalAttack += getTotal(defendingPlayer.charge);
-
-	console.log("trying to attack " + defendingPlayer.toString() + " with " + totalAttack);
+	totalAttack += getTotal(attackingPlayer.charge);
 
 	// if totalAttack goes through the shield
 	const shieldValue = getCardValue(defendingPlayer.shield[0]);
+
+	console.log("trying to attack " + shieldValue + " with " + totalAttack);
+
 	if (totalAttack > shieldValue) {
 		// losingPoints are remaining points after shield absorption
 		const losingPoints = totalAttack - shieldValue;
@@ -156,36 +202,61 @@ function attackKnight(attackingPlayer, defendingPlayer) {
 		const remainingHp = Math.max(0, totalHp - losingPoints);
 
 		// if remainingHp is zero, that player loses the game, discarding their cards
-		if (remainingHp == 0) {
-			// discarding hp cards
-			console.log(defendingPlayer.name + " loses their health points");
-			while (defendingPlayer.hp.length > 0) {
+		if (remainingHp <= 0) {
+			// discard all defendingPlayer hp
+			while(defendingPlayer.hp.length > 0) {
 				moveCard(defendingPlayer.hp, discardPile);
 			}
-			// discarding their shield
-			console.log(defendingPlayer.name + " loses their shield");
-			moveCard(defendingPlayer.shield, discardPile);
-			// remove the player from the game
-			console.log(defendingPlayer.name + " loses the game");
 			checkPlayerDead(defendingPlayer);
+			checkPlayerWon();
 		} else {
 			// try replacing only one card to match the new hp
+			let replacementValue = 0;
 		    for (let i = 0; i < defendingPlayer.hp.length; i++) {
-		        const unchangedTotal = getTotal(defendingPlayer.hp) - getCardValue(defendingPlayer.hp[i]);
-		
-		        const replacementValue = remainingHp - unchangedTotal;
+				replacementValue = getCardValue(defendingPlayer.hp[i]) - losingPoints;
+
 				// if replacementValue can be a card
-		        if (replacementValue >= 1 && replacementValue <= 13) {
-		            let replacementCard = findReplacementCardFor(replacementValue);
-					if (replacementCard[1] > 0) {
-						moveCard(defendingPlayer.hp, discardPile, defendingPlayer.hp[i]);
-						moveCard(replacementCard[0], defendingPlayer.hp, replacementCard[1]);
-					} else {
-						
-					}
+				if (replacementValue >= 1 && replacementValue <= 13) {
+					// discard one card from defending player hp
+					moveCard(defendingPlayer.hp, discardPile, defendingPlayer.hp[i]);
 					break;
-		        }
-		    }
+				}
+			}
+			// one card isn't enough
+			if (replacementValue <= 0) {
+				// discard all defendingPlayer hp
+				while(defendingPlayer.hp.length > 0) {
+					moveCard(defendingPlayer.hp, discardPile);
+				}
+				replacementValue = remainingHp;
+			}
+			console.log("totalHp: " + totalHp + " losingPoints: " + losingPoints + " replacement value: " + replacementValue);
+
+			// find replacement card(s)
+			let replacementCards = [];
+			// search for a card in discardPile / drawPile
+			const replacementCard = findReplacementCardFor(replacementValue);
+			// replacement card hasn't been found in either discardPile or drawPile, we need to search for a pair of cards
+			if (replacementCard[1] < 0) {
+				const pairs = generatePairsFrom(replacementValue);
+				for (let p = 0; p < pairs.length; p++) {
+					const firstCard = findReplacementCardFor(pairs[p]);
+					const secondCard = findReplacementCardFor(pairs[p]);
+					// this pair is a fit replacement
+					if (firstCard[1] > 0 && secondCard[1] > 0) {
+						replacementCards.push(firstCard);
+						replacementCards.push(secondCard);
+						break;
+					}
+				}
+			} else {
+				replacementCards.push(replacementCard);
+			}
+			
+			// add replacement card(s) to defendingPlayer hp
+			for (let c = 0; c < replacementCards.length; c++) {
+				moveCard(replacementCards[c][0], defendingPlayer.hp, replacementCards[c][1]);
+			}
 		}
 	} else {
 		console.log ("nothing happens...");
@@ -193,6 +264,10 @@ function attackKnight(attackingPlayer, defendingPlayer) {
 
 	// discarding the attack card
 	moveCard(drawPile, discardPile);
+	// discarding all charges
+	while(attackingPlayer.charge.length > 0) {
+		moveCard(attackingPlayer.charge, discardPile);
+	}
 
 	// defending player looses all their charges on an attack
 	while(defendingPlayer.charge.length > 0) {
@@ -200,11 +275,25 @@ function attackKnight(attackingPlayer, defendingPlayer) {
 	}
 }
 
+// empty player : discard all their cards
+function emptyPlayer(player) {
+	while(player.shield.length > 0) { moveCard(player.shield, discardPile); }
+	while(player.hp.length > 0) { moveCard(player.hp, discardPile); }
+	while(player.charge.length > 0) {moveCard(player.charge, discardPile); }
+	return player;
+}
+
 // check if a player has lost
 function checkPlayerDead(player) {
-	if(player.hp.length == 0) {
+	if(getTotal(player.hp) == 0) {
+		emptyPlayer(player);
 		const indexToRemove = players.indexOf(player);
+		// if the dead player would have been next, jump to the next one
+		if (activePlayerIndex + 1 == indexToRemove) {
+			nextPLayer();
+		}
 		const playerRemoved = players.splice(indexToRemove, 1)[0];
+		activePlayerIndex = activePlayerIndex % players.length;
 		return true;
 	}
 	else return false;
@@ -216,12 +305,18 @@ function checkPlayerWon() {
 	else return false;
 }
 
+// switch to the next player
+function nextPLayer() {
+	activePlayerIndex = (activePlayerIndex + 1) % players.length;
+}
+
 // init game
 function initGame(numberOfPlayers) {
 	// init players
 	for(let p = 1; p <= numberOfPlayers; p++) {
 		players.push(new Player("Player "+p));
 	}
+	activePlayerIndex = 0;
 	// shuffle cards
 	drawPile = shuffle(drawPile);
 	//console.log(drawPile);
@@ -232,6 +327,47 @@ function initGame(numberOfPlayers) {
 		moveCard(drawPile, players[p].shield);
 	}
 }
+
+// check if a point (x,y) is in a box (x1, y1, x2, y2)
+function isPointInBox(point, box) {
+	// point is [x, y];
+	// box is [x1, y1, x2, y2];
+	//console.log("checking "+point+" into "+box);
+	if(point[0] >= box[0] && point[0] <= box[2] && point[1] >= box[1] && point[1] <= box[3]) return true;
+	else return false;
+}
+
+// action on click
+var click=function(event) {
+	const clickPoint = [event.clientX, event.clientY];
+	if (GAMESTATE == 0) {
+		const attackBox = [attackPos.x, attackPos.y, attackPos.x+attackSprite.w/2, attackPos.y+attackSprite.h/2];
+		const changeShieldBox = [shieldPos.x, shieldPos.y, shieldPos.x+shieldSprite.w/2, shieldPos.y+shieldSprite.h/2];
+		const chargeBox = [chargePos.x, chargePos.y, chargePos.x+chargeSprite.w/2, chargePos.y+chargeSprite.h/2];
+
+		if (isPointInBox(clickPoint, attackBox)) { GAMESTATE = 1; }
+		else if (isPointInBox(clickPoint, changeShieldBox)) { GAMESTATE = 2; }
+		else if (isPointInBox(clickPoint, chargeBox)) { GAMESTATE = 3; }
+
+	} else if (GAMESTATE > 0 && GAMESTATE <= 3) {
+		for (let p=0; p < players.length; p++) {
+			let offset = {x:0, y:0};
+			offset.x = (p < 2) ? 50 : 550;
+			offset.y = (p % 2 === 0) ? 400 : 10;
+			const pBox = [offset.x, offset.y, offset.x+250, offset.y+200];
+
+			if (isPointInBox(clickPoint, pBox)) {
+				if (GAMESTATE === 1) { attackKnight(players[activePlayerIndex], players[p]); }
+				else if (GAMESTATE === 2) { changeShield(players[p]); }
+				else if (GAMESTATE === 3) { chargeKnight(players[p]); }
+				nextPLayer();
+				GAMESTATE = 0;
+			}
+		}
+	}
+}
+// make the canvas clickable
+c.onclick=click;
 
 /*
 var init=function(lvl) {
@@ -370,18 +506,98 @@ var updateClicks=function() {
 }
 */
 
+// can draw rotated images
+function drawImage(ctx, image, srcX, srcY, srcW, srcH, destX, destY, destW, destH, degrees){
+  ctx.save();
+  ctx.translate(destX+destW/2, destY+destH/2);
+  ctx.rotate(degrees*Math.PI/180.0);
+  ctx.translate(-destX-destW/2, -destY-destH/2);
+  ctx.drawImage(image, srcX, srcY, srcW, srcH, destX, destY, destW, destH);
+  ctx.restore();
+}
+
 // drawing cards on canvas
 let render=function() {
 	if(sprReady) {
 		ctx.fillStyle = "white";
 		ctx.fillRect(0, 0, 800, 600);
 		ctx.fillStyle = "black";
-		//ctx.drawImage(spr, 64+64*cardX, 64*cardY, 64, 64, offsetX, offsetY, 64, 64);
-		for (let p = 0; p < players.length; p++) {
-			ctx.fillText(players[p].toString(), 50, 50*(p+1));
+		const cardSizeOnScreen = {width:cardSize.width/2, height:cardSize.height/2};
+		// drawPile
+		const drawPilePos = {x:410, y:250 - cardSizeOnScreen.height/2};
+		ctx.drawImage(spr, cardFaceDown.x, cardFaceDown.y, cardSize.width, cardSize.height, drawPilePos.x, drawPilePos.y, cardSizeOnScreen.width, cardSizeOnScreen.height);
+		// discardPile
+		if (discardPile.length > 0) {
+			const discardPilePos = {x:400 - cardSizeOnScreen.width, y:250 - cardSizeOnScreen.height/2};
+			const lastDiscardPileCardSprite = getSpriteCoordFor(discardPile[discardPile.length-1]);
+			ctx.drawImage(spr, lastDiscardPileCardSprite.x, lastDiscardPileCardSprite.y, cardSize.width, cardSize.height, discardPilePos.x, discardPilePos.y, cardSizeOnScreen.width, cardSizeOnScreen.height);
 		}
-		ctx.fillText("drawPile: length " + drawPile.length + ", top " + getCardValue(drawPile[0]) + " of " + deck[drawPile[0]].color, 50, 150);
-		ctx.fillText("discardPile: length " + discardPile.length, 50, 200);
+		
+		// draw actions or choose player
+		if (GAMESTATE == 0) {
+			// draw attack
+			ctx.drawImage(spr, attackSprite.x, attackSprite.y, attackSprite.w, attackSprite.h, attackPos.x, attackPos.y, attackSprite.w/2, attackSprite.h/2);
+			// draw shield
+			ctx.drawImage(spr, shieldSprite.x, shieldSprite.y, shieldSprite.w, shieldSprite.h, shieldPos.x, shieldPos.y, shieldSprite.w/2, shieldSprite.h/2);
+			// draw charge
+			ctx.drawImage(spr, chargeSprite.x, chargeSprite.y, chargeSprite.w, chargeSprite.h, chargePos.x, chargePos.y, chargeSprite.w/2, chargeSprite.h/2);
+			// text
+			ctx.fillText("Choose an action", 330, 330);
+		} else if (GAMESTATE >= 1 && GAMESTATE <= 3) {
+			ctx.fillText("Choose a player", 330, 330);
+
+			if (GAMESTATE == 1) {
+				// draw attack
+				ctx.drawImage(spr, attackSprite.x, attackSprite.y, attackSprite.w, attackSprite.h, attackPos.x, attackPos.y, attackSprite.w/2, attackSprite.h/2);
+			} else if (GAMESTATE == 2) {
+				// draw shield
+				ctx.drawImage(spr, shieldSprite.x, shieldSprite.y, shieldSprite.w, shieldSprite.h, shieldPos.x, shieldPos.y, shieldSprite.w/2, shieldSprite.h/2);
+			} else if (GAMESTATE == 3) {
+				// draw charge
+				ctx.drawImage(spr, chargeSprite.x, chargeSprite.y, chargeSprite.w, chargeSprite.h, chargePos.x, chargePos.y, chargeSprite.w/2, chargeSprite.h/2);
+			}
+		}
+		
+		// draw players
+		for (let p = 0; p < players.length; p++) {
+			let offset = {x:0, y:0};
+			offset.x = (p < 2) ? 50 : 550;
+			offset.y = (p % 2 === 0) ? 400 : 10;
+
+			if(p === activePlayerIndex) {
+				ctx.fillStyle = playersColors[p];
+				ctx.fillRect(offset.x-5,offset.y+90,20,20);
+				ctx.fillStyle = "white";
+			}
+
+			let hpCardsPos = {x:offset.x+40, y:0};
+			hpCardsPos.y = (p % 2 === 0) ? offset.y + (200 - 10 - 78) : 10;
+
+			for (let hpCard = 0; hpCard < players[p].hp.length; hpCard++) {
+				const hpCardCoord = getSpriteCoordFor(players[p].hp[hpCard]);
+				ctx.drawImage(spr, hpCardCoord.x, hpCardCoord.y, cardSize.width, cardSize.height, hpCardsPos.x + hpCard*71.5, hpCardsPos.y, cardSizeOnScreen.width, cardSizeOnScreen.height);
+			}
+
+			// draw shield
+			let shieldCardPos = {x:offset.x + 40 + 65 - cardSizeOnScreen.width/2, y:0};
+			shieldCardPos.y = (p % 2 === 0) ? offset.y + (200 - 10 - 78 - 10 - 78) : 10 + (78 + 10);
+			const shieldCardCoord = getSpriteCoordFor(players[p].shield[0]);
+			drawImage(ctx, spr, shieldCardCoord.x, shieldCardCoord.y, cardSize.width, cardSize.height, shieldCardPos.x, shieldCardPos.y, cardSizeOnScreen.width, cardSizeOnScreen.height, 90);
+			//ctx.drawImage(spr, shieldCardCoord.x, shieldCardCoord.y, cardSize.width, cardSize.height, shieldCardPos.x, shieldCardPos.y, cardSizeOnScreen.width, cardSizeOnScreen.height);
+			
+			// draw charge
+			if (players[p].charge.length > 0) {
+				let chargeCardPos = {x:offset.x + 40 + 65 + 65, y:0};
+				chargeCardPos.y = (p % 2 == 0) ? offset.y + (200 - 10 - 78 - 10 - 78) : 10 + (78 + 10);
+				ctx.drawImage(spr, cardFaceDown.x, cardFaceDown.y, cardSize.width, cardSize.height, chargeCardPos.x, chargeCardPos.y, cardSizeOnScreen.width, cardSizeOnScreen.height);
+				ctx.fillStyle = "goldenrod";
+				ctx.fillRect(chargeCardPos.x-2, chargeCardPos.y-2, 29, 29);
+				ctx.fillStyle = "white";
+				ctx.fillRect(chargeCardPos.x, chargeCardPos.y, 25, 25);
+				ctx.fillStyle = "black";
+				ctx.fillText(players[p].charge.length, chargeCardPos.x+7, chargeCardPos.y+20);
+			}
+		}
 	}
 }
 
