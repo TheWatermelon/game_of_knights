e0560e2f83b8c952ebb9cf0edfb9d95cdd1b10d8
TheWatermelon <TheWatermelon@users.noreply.github.com>
2026-09-03 13:01:51 +0200

    Update render.js

--- js/render.js
+++ js/render.js
@@ -1,3 +1,13 @@
 class Renderer {
     constructor() {}
-} 
+}
+
+class AnimationManager {
+    constructor() {
+        this.animations = [];
+    }
+
+    function addAnimation() {}
+
+    function updateAnimations() {}
+}
